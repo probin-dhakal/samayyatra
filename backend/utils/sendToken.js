3cb50a45a2495ff04819b3a201642b7390633317
probin-dhakal <154975813+probin-dhakal@users.noreply.github.com>
2025-01-09 19:01:27 +0530

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const sendToken = (user, statusCode, res, message = "Success") => {
-  // Generate JWT token
+ 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "1h",
   });
   // console.log("Generated token:", token);
 
-  // Set the token as an HTTP-only cookie
+  
 res.cookie("token", token, {
     httpOnly: true, // Prevent client-side JS from accessing the cookie
     secure: true, // Use true only in production (HTTPS)
-    maxAge: 7200000, // 1 hour
+    maxAge: 10800000, // 3 hour
     sameSite: "None", // Adjust based on cross-origin use cases
 });
 
