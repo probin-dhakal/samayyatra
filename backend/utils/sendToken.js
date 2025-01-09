c2231e616ebaa5850ab6a3de209f27ed01189083
probin-dhakal <154975813+probin-dhakal@users.noreply.github.com>
2025-01-09 23:38:42 +0530

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -8,14 +8,20 @@ export const sendToken = (user, statusCode, res, message = "Success") => {
   // console.log("Generated token:", token);
 
   
-res.cookie("token", token, {
-  httpOnly: true,  // Prevent access by JavaScript
-  secure: true,  // Ensure it's secure in production
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 day
-  sameSite: "None",  // Required for cross-origin cookies
-  path: "/",  // Path to make the cookie available globally
-});
+// res.cookie("token", token, {
+//   httpOnly: true,  // Prevent access by JavaScript
+//   secure: true,  // Ensure it's secure in production
+//   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 day
+//   sameSite: "None",  // Required for cross-origin cookies
+//   path: "/",  // Path to make the cookie available globally
+// });
 
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", // Enable only in production
+  sameSite: "None",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
   
   // Send response
