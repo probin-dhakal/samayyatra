34f57adb59fd2085511d50efcc8a6e51107e1615
probin-dhakal <154975813+probin-dhakal@users.noreply.github.com>
2025-01-10 09:08:34 +0530

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -17,7 +17,7 @@ export const sendToken = (user, statusCode, res, message = "Success") => {
 // });
 
 res.cookie("token", token, {
-  httpOnly: true,
+  // httpOnly: true,
   secure: process.env.NODE_ENV === "production", // Enable only in production
   sameSite: "None",
   maxAge: 7 * 24 * 60 * 60 * 1000,
